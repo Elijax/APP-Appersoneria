eeed84b99d7823260c88b60b4a8ac7c1d88b7949
Moises Valera <elijax92@hotmail.com>
2015-01-06 14:22:58 -0500

    last commit

--- platforms/android/CordovaLib/ant-build/AndroidManifest.xml.d
+++ platforms/android/CordovaLib/ant-build/AndroidManifest.xml.d
@@ -1,2 +1,2 @@
-C:\wamp\www\Appersoneria\appersoneria Kurten\APPersoneria-APP\APP-Appersoneria\platforms\android\CordovaLib\ant-build\AndroidManifest.xml : \
-C:\wamp\www\Appersoneria\appersoneria Kurten\APPersoneria-APP\APP-Appersoneria\platforms\android\CordovaLib\AndroidManifest.xml \
+C:\wamp\www\Appersoneria\appersoneria Kurten\APPersoneria-APP\APP-Appersoneria\APP-Appersoneria\platforms\android\CordovaLib\ant-build\AndroidManifest.xml : \
+C:\wamp\www\Appersoneria\appersoneria Kurten\APPersoneria-APP\APP-Appersoneria\APP-Appersoneria\platforms\android\CordovaLib\AndroidManifest.xml \
--- platforms/android/ant-build/classes.dex.d
+++ platforms/android/ant-build/classes.dex.d
@@ -1,18 +1,18 @@
-C:\wamp\www\Appersoneria\appersoneria Kurten\APPersoneria-APP\APP-Appersoneria\platforms\android\ant-build\classes.dex : \
-C:\wamp\www\Appersoneria\appersoneria Kurten\APPersoneria-APP\APP-Appersoneria\platforms\android\ant-build\classes\com\app\APPersoneria\BuildConfig.class \
-C:\wamp\www\Appersoneria\appersoneria Kurten\APPersoneria-APP\APP-Appersoneria\platforms\android\ant-build\classes\com\app\APPersoneria\CordovaApp.class \
-C:\wamp\www\Appersoneria\appersoneria Kurten\APPersoneria-APP\APP-Appersoneria\platforms\android\ant-build\classes\com\app\APPersoneria\R$attr.class \
-C:\wamp\www\Appersoneria\appersoneria Kurten\APPersoneria-APP\APP-Appersoneria\platforms\android\ant-build\classes\com\app\APPersoneria\R$drawable.class \
-C:\wamp\www\Appersoneria\appersoneria Kurten\APPersoneria-APP\APP-Appersoneria\platforms\android\ant-build\classes\com\app\APPersoneria\R$string.class \
-C:\wamp\www\Appersoneria\appersoneria Kurten\APPersoneria-APP\APP-Appersoneria\platforms\android\ant-build\classes\com\app\APPersoneria\R$xml.class \
-C:\wamp\www\Appersoneria\appersoneria Kurten\APPersoneria-APP\APP-Appersoneria\platforms\android\ant-build\classes\com\app\APPersoneria\R.class \
-C:\wamp\www\Appersoneria\appersoneria Kurten\APPersoneria-APP\APP-Appersoneria\platforms\android\ant-build\classes\com\ionicframework\appersoneria731340\BuildConfig.class \
-C:\wamp\www\Appersoneria\appersoneria Kurten\APPersoneria-APP\APP-Appersoneria\platforms\android\ant-build\classes\com\ionicframework\appersoneria731340\CordovaApp.class \
-C:\wamp\www\Appersoneria\appersoneria Kurten\APPersoneria-APP\APP-Appersoneria\platforms\android\ant-build\classes\com\ionicframework\appersoneria731340\R$attr.class \
-C:\wamp\www\Appersoneria\appersoneria Kurten\APPersoneria-APP\APP-Appersoneria\platforms\android\ant-build\classes\com\ionicframework\appersoneria731340\R$drawable.class \
-C:\wamp\www\Appersoneria\appersoneria Kurten\APPersoneria-APP\APP-Appersoneria\platforms\android\ant-build\classes\com\ionicframework\appersoneria731340\R$string.class \
-C:\wamp\www\Appersoneria\appersoneria Kurten\APPersoneria-APP\APP-Appersoneria\platforms\android\ant-build\classes\com\ionicframework\appersoneria731340\R$xml.class \
-C:\wamp\www\Appersoneria\appersoneria Kurten\APPersoneria-APP\APP-Appersoneria\platforms\android\ant-build\classes\com\ionicframework\appersoneria731340\R.class \
-C:\wamp\www\Appersoneria\appersoneria Kurten\APPersoneria-APP\APP-Appersoneria\platforms\android\ant-build\classes\org\apache\cordova\networkinformation\NetworkManager$1.class \
-C:\wamp\www\Appersoneria\appersoneria Kurten\APPersoneria-APP\APP-Appersoneria\platforms\android\ant-build\classes\org\apache\cordova\networkinformation\NetworkManager.class \
-C:\wamp\www\Appersoneria\appersoneria Kurten\APPersoneria-APP\APP-Appersoneria\platforms\android\ant-build\dexedLibs\classes-ea583f8e5c350e0ec378667362cadf73.jar \
+C:\wamp\www\Appersoneria\appersoneria Kurten\APPersoneria-APP\APP-Appersoneria\APP-Appersoneria\platforms\android\ant-build\classes.dex : \
+C:\wamp\www\Appersoneria\appersoneria Kurten\APPersoneria-APP\APP-Appersoneria\APP-Appersoneria\platforms\android\ant-build\classes\com\app\APPersoneria\BuildConfig.class \
+C:\wamp\www\Appersoneria\appersoneria Kurten\APPersoneria-APP\APP-Appersoneria\APP-Appersoneria\platforms\android\ant-build\classes\com\app\APPersoneria\CordovaApp.class \
+C:\wamp\www\Appersoneria\appersoneria Kurten\APPersoneria-APP\APP-Appersoneria\APP-Appersoneria\platforms\android\ant-build\classes\com\app\APPersoneria\R$attr.class \
+C:\wamp\www\Appersoneria\appersoneria Kurten\APPersoneria-APP\APP-Appersoneria\APP-Appersoneria\platforms\android\ant-build\classes\com\app\APPersoneria\R$drawable.class \
+C:\wamp\www\Appersoneria\appersoneria Kurten\APPersoneria-APP\APP-Appersoneria\APP-Appersoneria\platforms\android\ant-build\classes\com\app\APPersoneria\R$string.class \
+C:\wamp\www\Appersoneria\appersoneria Kurten\APPersoneria-APP\APP-Appersoneria\APP-Appersoneria\platforms\android\ant-build\classes\com\app\APPersoneria\R$xml.class \
+C:\wamp\www\Appersoneria\appersoneria Kurten\APPersoneria-APP\APP-Appersoneria\APP-Appersoneria\platforms\android\ant-build\classes\com\app\APPersoneria\R.class \
+C:\wamp\www\Appersoneria\appersoneria Kurten\APPersoneria-APP\APP-Appersoneria\APP-Appersoneria\platforms\android\ant-build\classes\com\ionicframework\appersoneria731340\BuildConfig.class \
+C:\wamp\www\Appersoneria\appersoneria Kurten\APPersoneria-APP\APP-Appersoneria\APP-Appersoneria\platforms\android\ant-build\classes\com\ionicframework\appersoneria731340\CordovaApp.class \
+C:\wamp\www\Appersoneria\appersoneria Kurten\APPersoneria-APP\APP-Appersoneria\APP-Appersoneria\platforms\android\ant-build\classes\com\ionicframework\appersoneria731340\R$attr.class \
+C:\wamp\www\Appersoneria\appersoneria Kurten\APPersoneria-APP\APP-Appersoneria\APP-Appersoneria\platforms\android\ant-build\classes\com\ionicframework\appersoneria731340\R$drawable.class \
+C:\wamp\www\Appersoneria\appersoneria Kurten\APPersoneria-APP\APP-Appersoneria\APP-Appersoneria\platforms\android\ant-build\classes\com\ionicframework\appersoneria731340\R$string.class \
+C:\wamp\www\Appersoneria\appersoneria Kurten\APPersoneria-APP\APP-Appersoneria\APP-Appersoneria\platforms\android\ant-build\classes\com\ionicframework\appersoneria731340\R$xml.class \
+C:\wamp\www\Appersoneria\appersoneria Kurten\APPersoneria-APP\APP-Appersoneria\APP-Appersoneria\platforms\android\ant-build\classes\com\ionicframework\appersoneria731340\R.class \
+C:\wamp\www\Appersoneria\appersoneria Kurten\APPersoneria-APP\APP-Appersoneria\APP-Appersoneria\platforms\android\ant-build\classes\org\apache\cordova\networkinformation\NetworkManager$1.class \
+C:\wamp\www\Appersoneria\appersoneria Kurten\APPersoneria-APP\APP-Appersoneria\APP-Appersoneria\platforms\android\ant-build\classes\org\apache\cordova\networkinformation\NetworkManager.class \
+C:\wamp\www\Appersoneria\appersoneria Kurten\APPersoneria-APP\APP-Appersoneria\APP-Appersoneria\platforms\android\ant-build\dexedLibs\classes-29b8f55813c87ef4edcf8d727d2e9540.jar \
